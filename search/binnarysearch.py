8824f54fa4b2c9746bf083a45faff0c6309a972e
jinson jijy <jinsonjijy50@gmail.com>
2025-05-28 08:38:10 +0530

    binnary search done and executed successfully

---
+++ search/binnarysearch.py
@@ -0,0 +1,14 @@
+def binnary(a,low,high,x):
+    """here in this functiion were are planning to implement the binnary search in the itrative manner this is done for better understadnding of the data structure and algorithms"""
+    while low<=high:
+        mid=low+(high-low)//2
+        if a[mid]==x:
+            return mid
+        elif a[mid]<x:
+            low=mid+1
+        else:
+            high=mid-1
+    return False
+arr=[23,45,41,47,67,89,100]
+a=binnary(arr,0,len(arr)-1,100)
+print(a)
