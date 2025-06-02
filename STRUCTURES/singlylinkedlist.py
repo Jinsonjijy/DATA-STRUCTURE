c5ca688add1f70ce3274719764f638ae6502a5db
jinson jijy <jinsonjijy50@gmail.com>
2025-06-02 21:59:25 +0530

    done some work to the linkedlist and i will do the rest of the work tommorow

--- STRUCTURES/singlylinkedlist.py
+++ STRUCTURES/singlylinkedlist.py
@@ -9,5 +9,22 @@ class linkedlist:
         new_node=Node(data)
         new_node=self.head
         self.head=new_node
+    def insertatindex(self,data,index):
+        if index==0:
+            self.inseratbegin(data)
+            return
+        position=0
+        current_node=self.head
+        while current_node is not None and position+1!=index:
+            position+=1
+            current_node=current_node.next
+        if current_node is not None:
+            new_node=Node(data)
+            new_node.next=current_node.next
+            current_node.next=new_node
+            
+        else:
+            print("index out of bound  not found")
+
 l=linkedlist()
 l.inseratbegin(23)
